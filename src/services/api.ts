cc6fddde7027e234990e596f284ff91683971489
saitama <saitama.com>
2026-01-06 23:31:14 +0300

    Backend linki guncellendi

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { InternalAxiosRequestConfig, AxiosError } from 'axios';
 
 // Varsayılan backend URL, deploy olduğunda değişebilir
-export const API_URL = 'http://localhost:3000/api';
+export const API_URL = 'http://rent-a-car-backend-6pfm.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_URL,
